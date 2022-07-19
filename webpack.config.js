31a6668b49a306a7cf10b171429844ef0a2ecf77
jmgirard <me@jmgirard.com>
2022-07-19 14:07:12 -0500

    Add core-js and plugins to try to get ES5 support

--- webpack.config.js
+++ webpack.config.js
@@ -3,24 +3,28 @@ module.exports = {
 	"mode": "production",
 	"output": {
 		"path": `${__dirname}/_extensions/lordicon/assets/js/`,
-		"filename": "bundle.js"
+		"filename": "bundle.js",
 	},
   "module": { 
-    "rules": [ { 
-      "test": /\.css$/, 
-      "use": [ "style-loader", "css-loader" ] 
-    }, 
-    { 
-      "test": /\.js$/, 
-      "exclude": /node_modules/, 
-      "use": { 
-        "loader": "babel-loader", 
-        "options": { 
-          "presets": [
-            [ "@babel/preset-env", { "targets": "defaults" } ]
-          ] 
-        } 
-      } 
-    } ] 
-  }
+    "rules": [ 
+		{ 
+		  "test": /\.css$/, 
+		  "use": [ "style-loader", "css-loader" ],
+		}, 
+		{ 
+		  "test": /\.js$/, 
+		  "exclude": /node_modules/, 
+		  "use": { 
+			"loader": "babel-loader", 
+			"options": { 
+			  "presets": [
+					["@babel/preset-env", { 
+						"debug": true,
+					}],
+				],
+			},
+		  }, 
+		}, 
+	], 
+  },
 };
