4dd3edc5668d245730f71cc13a6d6ba83ee19fe6
jmgirard <me@jmgirard.com>
2023-02-13 20:01:16 -0600

    Remove body CSS

--- _extensions/lordicon/lordicon.lua
+++ _extensions/lordicon/lordicon.lua
@@ -1,7 +1,7 @@
 function ensureHtmlDeps()
   quarto.doc.addHtmlDependency({
     name = 'lord-icon-bundle',
-    version = '3.4.2',
+    version = '3.4.4',
     scripts = {'assets/js/bundle.js'}
   })
 end
