d88dca12e06ffdbc6325188f84a65c46e52921c9
Jeffrey Girard <me@jmgirard.com>
2024-11-30 21:39:18 -0600

    update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,31 @@
-const TerserPlugin = require('terser-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
-	"entry": "./src/main.js",
-	"output": {
-		"path": `${__dirname}/_extensions/lordicon/assets/js/`,
-		"filename": "bundle.js",
-	},
-  "module": { 
-    "rules": [ 
-		  { 
-  		  "test": /\.js$/, 
-  		  "exclude": /node_modules/, 
-  		  "use": { 
-  			  "loader": "babel-loader", 
-  			  "options": { 
-  			    "presets": ["@babel/preset-env"],
-  			  },
-  			},
-  		}, 
+  entry: "./src/main.js",
+  output: {
+    path: `${__dirname}/_extensions/lordicon/assets/js/`,
+    filename: "bundle.js",
+  },
+  module: { 
+    rules: [ 
+      { 
+      test: /\.js$/, 
+      exclude: /node_modules/, 
+      use: { 
+        loader: "babel-loader", 
+          options: { 
+            presets: ["@babel/preset-env"],
+            plugins: [
+              "@babel/plugin-transform-classes"
+            ],
+          },
+        },
+      }, 
     ], 
   },
   optimization: {
     minimize: true,
     minimizer: [new TerserPlugin()],
-  },
-  mode: 'production'
+	},
+  mode: 'production',
 };
